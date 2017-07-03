a714698eb1dd27fac12018b0409eadd3ebf62c1c
ksysdkci <pengbin@kingsoft.com>
2017-07-03 21:06:37 +0800

    update to v2.4.1

--- prebuilt/include/KSYPlayer/KSYMoviePlayerController.h
+++ prebuilt/include/KSYPlayer/KSYMoviePlayerController.h
@@ -578,6 +578,16 @@ typedef void (^KSYPlyTextureBlock)(GLuint texId, int width, int height, double p
 @property (nonatomic, readonly) KSYNetworkStatus networkStatus;
 
 /**
+ @abstract 设置播放速度，取值范围(0.5~2.0)，默认1.0
+ @warning 该属性目前只支持软解播放，不支持硬解播放，且播放直播流时请勿修改该属性
+ @warning 该属性由金山云引入，不是原生系统接口
+ @since Available in KSYMoviePlayerController 2.4.1 and later.
+ */
+@property (nonatomic) float playbackSpeed;
+
+
+
+/**
  @abstract timeout指定拉流超时时间,单位是秒
  @param prepareTimeout 建立链接超时时间，默认值是10秒
  @param readTimeout 拉流超时时间，默认值是30秒
